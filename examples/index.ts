aec3f281e4372c11f4515fe54396855028c7fc3d
Marko Grizelj <marko.grizelj03@gmail.com>
2022-08-12 09:58:50 +0200

    chore: changed indent style

--- examples/index.ts
+++ examples/index.ts
@@ -2,27 +2,27 @@ import { Permissions, Schema } from '../src';
 import { JsonPermissions } from '../lib/types';
 
 const jsonSchema = {
-	sendMessage: true,
-	readMessageHistory: true,
-	deleteMessages: false,
-	createThreads: true,
-	sendEmoji: true,
-	sendFiles: true,
-	editChannels: false,
-	createChannels: false,
-	deleteChannels: false,
-	kickUsers: false,
-	banUsers: false,
-	inviteUsers: true,
-	type: {
-		default: 'user',
-		fields: [
-			'user',
-			'moderator',
-			'admin',
-			'owner',
-		],
-	},
+  sendMessage: true,
+  readMessageHistory: true,
+  deleteMessages: false,
+  createThreads: true,
+  sendEmoji: true,
+  sendFiles: true,
+  editChannels: false,
+  createChannels: false,
+  deleteChannels: false,
+  kickUsers: false,
+  banUsers: false,
+  inviteUsers: true,
+  type: {
+    default: 'user',
+    fields: [
+      'user',
+      'moderator',
+      'admin',
+      'owner',
+    ],
+  },
 };
 
 const schema: Schema<typeof jsonSchema> = new Schema(jsonSchema);
@@ -34,41 +34,41 @@ const pString = 'OwgA';
 const pFromString: Permissions<typeof jsonSchema> = Permissions.fromBase64(pString, schema);
 
 if (pFromString.is(schema.fields.sendMessage, false)) {
-	console.log('This user can\'t send messages');
+  console.log('This user can\'t send messages');
 }
 
 if (pFromString.is(schema.fields.type, schema.fields.type.admin)) {
-	console.log('This user is admin');
+  console.log('This user is admin');
 }
 
 const json: JsonPermissions = {
-	sendMessage: true,
-	readMessageHistory: true,
-	deleteMessages: false,
-	createThreads: true,
-	sendEmoji: true,
-	sendFiles: true,
-	editChannels: false,
-	createChannels: false,
-	deleteChannels: false,
-	kickUsers: false,
-	banUsers: false,
-	inviteUsers: true,
-	type: 'user',
+  sendMessage: true,
+  readMessageHistory: true,
+  deleteMessages: false,
+  createThreads: true,
+  sendEmoji: true,
+  sendFiles: true,
+  editChannels: false,
+  createChannels: false,
+  deleteChannels: false,
+  kickUsers: false,
+  banUsers: false,
+  inviteUsers: true,
+  type: 'user',
 };
 
 const pFromJson: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
 
 if (pFromString.is(schema.fields.type, schema.fields.type.admin)) {
-	console.log('This user is admin');
+  console.log('This user is admin');
 }
 
 if (pFromString.is(schema.fields.kickUsers, true)) {
-	console.log('This user can kick users');
+  console.log('This user can kick users');
 }
 
 if (pFromString.is(schema.fields.banUsers, true)) {
-	console.log('This user can ban users');
+  console.log('This user can ban users');
 }
 
 pFromJson.set(schema.fields.kickUsers, true);
@@ -76,22 +76,22 @@ pFromJson.set(schema.fields.banUsers, true);
 pFromJson.set(schema.fields.type, schema.fields.type.admin);
 
 if (pFromString.is(schema.fields.type, schema.fields.type.admin)) {
-	console.log('This user is admin');
+  console.log('This user is admin');
 }
 
 if (pFromString.is(schema.fields.kickUsers, true)) {
-	console.log('This user can kick users');
+  console.log('This user can kick users');
 }
 
 if (pFromString.is(schema.fields.banUsers, true)) {
-	console.log('This user can ban users');
+  console.log('This user can ban users');
 }
 
 const pJson: JsonPermissions = pFromJson.toJson();
 console.log(pJson);
 
 if (pJson.createThreads) {
-	console.log('This user can create threads.');
+  console.log('This user can create threads.');
 }
 
 const pFromCtor: Permissions<typeof jsonSchema> = new Permissions(0n, schema);
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,81 +2,81 @@ import { Permissions, Schema } from './index';
 import { JsonPermissions } from './types';
 
 const jsonSchema = {
-	writeAccess: false,
-	readAccess: true,
-	deleteAccess: false,
-	type: {
-		default: 'user',
-		fields: [
-			'moderator',
-			'admin',
-			'user',
-			'guest',
-		],
-	},
-	premium: true,
-	visible: true,
+  writeAccess: false,
+  readAccess: true,
+  deleteAccess: false,
+  type: {
+    default: 'user',
+    fields: [
+      'moderator',
+      'admin',
+      'user',
+      'guest',
+    ],
+  },
+  premium: true,
+  visible: true,
 };
 
 const schema: Schema<typeof jsonSchema> = new Schema(jsonSchema);
 
 const json: JsonPermissions = {
-	writeAccess: false,
-	readAccess: true,
-	deleteAccess: false,
-	type: 'user',
-	premium: true,
-	visible: true,
+  writeAccess: false,
+  readAccess: true,
+  deleteAccess: false,
+  type: 'user',
+  premium: true,
+  visible: true,
 };
 
 const validBase64: string = Permissions.fromJson(json, schema).toBase64();
 
 test('create default', () => {
-	const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
+  const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
 
-	expect(schema.createDefault().valueOf()).toBe(permissions.valueOf());
+  expect(schema.createDefault().valueOf()).toBe(permissions.valueOf());
 });
 
 test('from/to json', () => {
-	const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
+  const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
 
-	expect(permissions.toJson()).toStrictEqual(json);
+  expect(permissions.toJson()).toStrictEqual(json);
 });
 
 test('from/to base64', () => {
-	const permissions: Permissions<typeof jsonSchema> = Permissions.fromBase64(validBase64, schema);
+  const permissions: Permissions<typeof jsonSchema> = Permissions.fromBase64(validBase64, schema);
 
-	expect(permissions.toBase64()).toBe(validBase64);
+  expect(permissions.toBase64()).toBe(validBase64);
 });
 
 test('is/set values', () => {
-	const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
+  const permissions: Permissions<typeof jsonSchema> = Permissions.fromJson(json, schema);
 
-	expect(permissions.is(schema.fields.writeAccess, true)).toBe(false);
-	expect(permissions.is(schema.fields.readAccess, true)).toBe(true);
-	expect(permissions.is(schema.fields.deleteAccess, true)).toBe(false);
-	expect(permissions.is(schema.fields.type, schema.fields.type.user)).toBe(true);
-	expect(permissions.is(schema.fields.premium, true)).toBe(true);
-	expect(permissions.is(schema.fields.visible, true)).toBe(true);
+  expect(permissions.is(schema.fields.writeAccess, true)).toBe(false);
+  expect(permissions.is(schema.fields.readAccess, true)).toBe(true);
+  expect(permissions.is(schema.fields.deleteAccess, true)).toBe(false);
+  expect(permissions.is(schema.fields.type, schema.fields.type.user)).toBe(true);
+  expect(permissions.is(schema.fields.premium, true)).toBe(true);
+  expect(permissions.is(schema.fields.visible, true)).toBe(true);
 
-	permissions.set(schema.fields.type, schema.fields.type.admin);
-	permissions.set(schema.fields.visible, false);
-	permissions.set(schema.fields.writeAccess, true);
-	permissions.set(schema.fields.deleteAccess, true);
+  permissions.set(schema.fields.type, schema.fields.type.admin);
+  permissions.set(schema.fields.visible, false);
+  permissions.set(schema.fields.writeAccess, true);
+  permissions.set(schema.fields.deleteAccess, true);
 
-	expect(permissions.is(schema.fields.writeAccess, true)).toBe(true);
-	expect(permissions.is(schema.fields.readAccess, true)).toBe(true);
-	expect(permissions.is(schema.fields.deleteAccess, true)).toBe(true);
-	expect(permissions.is(schema.fields.type, schema.fields.type.admin)).toBe(true);
-	expect(permissions.is(schema.fields.premium, true)).toBe(true);
-	expect(permissions.is(schema.fields.visible, true)).toBe(false);
+  expect(permissions.is(schema.fields.writeAccess, true)).toBe(true);
+  expect(permissions.is(schema.fields.readAccess, true)).toBe(true);
+  expect(permissions.is(schema.fields.deleteAccess, true)).toBe(true);
+  expect(permissions.is(schema.fields.type, schema.fields.type.admin)).toBe(true);
+  expect(permissions.is(schema.fields.premium, true)).toBe(true);
+  expect(permissions.is(schema.fields.visible, true)).toBe(false);
 });
 
 test('string length', () => {
-	expect(() => Permissions.fromBase64('a', schema)).toThrow('Invalid string input, expected string of length 4, received string of length 1.');
-	expect(() => Permissions.fromBase64('aadasdad', schema)).toThrow('Invalid string input, expected string of length 4, received string of length 8.');
+  expect(() => Permissions.fromBase64('a', schema)).toThrow('Invalid string input, expected string of length 4, received string of length 1.');
+  expect(() => Permissions.fromBase64('aadasdad', schema)).toThrow('Invalid string input, expected string of length 4, received string of length 8.');
 });
 
 test('base64', () => {
-	expect(() => Permissions.fromBase64('aa?d', schema)).toThrow('Invalid base64 string');
+  expect(() => Permissions.fromBase64('aa?d', schema)).toThrow('Invalid base64 string');
 });
